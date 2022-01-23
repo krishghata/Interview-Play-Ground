9323eaa8570b45b13701aee2e78ce695c13720f6
Krishnendu <web.way2krish@gmail.com>
2022-01-23 13:21:34 +0530

    init

---
+++ src/main/java/io/eightfolds/spring/InterviewPlayGroundApplication.java
@@ -0,0 +1,13 @@
+package io.eightfolds.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewPlayGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewPlayGroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/eightfolds/spring/InterviewPlayGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eightfolds.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewPlayGroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
